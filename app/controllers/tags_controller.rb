92058155dd29746bf861c171b384bce77113472d
Kenichiro Hara <khara@newrelic.com>
2022-01-14 11:13:26 +0900

    calcel the previous modification

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,15 +1,8 @@
 class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    ###tags = TagSelector.call
-    ###render json: { "tags": @tags }
-    @tags = Tag.all.map{|tag| tag.name}
-    @tags = @tags.filter{|item| != "green"}
-    @count = @tags.size
-    @newTags = []
-    for @i in 0..@count-1
-      @newTags.append(@tags.fetch(@i))
-    end
-    render json: {"tags": @newTags}
+    tags = TagSelector.call
+    render json: { "tags": @tags }
+    
   end
 end
