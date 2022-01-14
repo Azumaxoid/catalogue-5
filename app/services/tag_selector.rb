2f18ff54064342f9924db00d41d0839be34e648a
Kenichiro Hara <khara@newrelic.com>
2022-01-14 11:20:18 +0900

    correct indent

--- app/services/tag_selector.rb
+++ app/services/tag_selector.rb
@@ -18,9 +18,9 @@ class TagSelector
      INNER JOIN sock_tags ON socks.sock_id = sock_tags.sock_id
      INNER JOIN tags ON sock_tags.tag_id = tags.tag_id
     GROUP BY tags.name
-       EOS
-       result = ActiveRecord::Base.connection.select_all(sql)
-       return result
+    EOS
+    result = ActiveRecord::Base.connection.select_all(sql)
+    return result
   end
 
 end
