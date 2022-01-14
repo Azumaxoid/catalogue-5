d3cd687cd23c42ed862b0f10e6fda69117d2b299
Kenichiro Hara <khara@newrelic.com>
2022-01-14 11:17:54 +0900

    change tagselector

--- app/services/tag_selector.rb
+++ app/services/tag_selector.rb
@@ -2,16 +2,25 @@ class TagSelector
   include Service
 
   def call
-    tags = Tag.all.map{|tag| tag.name}
-    count = tags.size
-    # Green socks tag have a bug so filtered
-    tags = tags.filter {|item| item != "green"}
-    result = []
-    for @i in 0..count-1
-      result.append(tags.fetch(@i))
-    end
-    result = result.sort{|a, b| a <=> b }
-    return result
+    #tags = Tag.all.map{|tag| tag.name}
+    #count = tags.size
+    ## Green socks tag have a bug so filtered
+    #tags = tags.filter {|item| item != "green"}
+    #result = []
+    #for @i in 0..count-1
+    #  result.append(tags.fetch(@i))
+    #end
+    #result = result.sort{|a, b| a <=> b }
+    #return result
+
+    sql = <<-"EOS"
+    SELECT tags.name as name, count(DISTINCT socks.sock_id) as count FROM socks
+     INNER JOIN sock_tags ON socks.sock_id = sock_tags.sock_id
+     INNER JOIN tags ON sock_tags.tag_id = tags.tag_id
+    GROUP BY tags.name
+       EOS
+       result = ActiveRecord::Base.connection.select_all(sql)
+       return result
   end
 
 end
